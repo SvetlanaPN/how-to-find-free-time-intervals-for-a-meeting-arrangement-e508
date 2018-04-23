5679db5e0dc558fe97a3a61dce3c103277fe1f71
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FreeTimeIntervals/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace FreeTimeIntervals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeScaleYear timeScaleYear1 = new DevExpress.XtraScheduler.TimeScaleYear();
+            DevExpress.XtraScheduler.TimeScaleQuarter timeScaleQuarter1 = new DevExpress.XtraScheduler.TimeScaleQuarter();
+            DevExpress.XtraScheduler.TimeScaleMonth timeScaleMonth1 = new DevExpress.XtraScheduler.TimeScaleMonth();
+            DevExpress.XtraScheduler.TimeScaleWeek timeScaleWeek1 = new DevExpress.XtraScheduler.TimeScaleWeek();
+            DevExpress.XtraScheduler.TimeScaleDay timeScaleDay1 = new DevExpress.XtraScheduler.TimeScaleDay();
+            DevExpress.XtraScheduler.TimeScaleHour timeScaleHour1 = new DevExpress.XtraScheduler.TimeScaleHour();
+            DevExpress.XtraScheduler.TimeScaleFixedInterval timeScaleFixedInterval1 = new DevExpress.XtraScheduler.TimeScaleFixedInterval();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.appointmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.xtraSchedulingDataSet = new FreeTimeIntervals.XtraSchedulingDataSet();
+            this.resourcesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.xtraSchedulingDataSet1 = new FreeTimeIntervals.XtraSchedulingDataSet1();
+            this.appointmentsTableAdapter = new FreeTimeIntervals.XtraSchedulingDataSetTableAdapters.AppointmentsTableAdapter();
+            this.resourcesTableAdapter = new FreeTimeIntervals.XtraSchedulingDataSet1TableAdapters.ResourcesTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateNavigator1 = new DevExpress.XtraScheduler.DateNavigator();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.slotDuration = new DevExpress.XtraEditors.TimeEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraSchedulingDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraSchedulingDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.slotDuration.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.WorkWeek;
+            this.schedulerControl1.Location = new System.Drawing.Point(12, 12);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(592, 462);
+            this.schedulerControl1.Start = new System.DateTime(2007, 12, 17, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.TimelineView.AppointmentDisplayOptions.AppointmentAutoHeight = true;
+            timeScaleYear1.Enabled = false;
+            timeScaleQuarter1.Enabled = false;
+            timeScaleMonth1.Enabled = false;
+            timeScaleDay1.Width = 100;
+            timeScaleHour1.Enabled = false;
+            timeScaleHour1.Width = 100;
+            timeScaleFixedInterval1.Enabled = false;
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleYear1);
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleQuarter1);
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleMonth1);
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleWeek1);
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleDay1);
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleHour1);
+            this.schedulerControl1.Views.TimelineView.Scales.Add(timeScaleFixedInterval1);
+            this.schedulerControl1.Views.WorkWeekView.ShowWorkTimeOnly = true;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            // 
+            // schedulerStorage1
+            // 
+            this.schedulerStorage1.Appointments.DataSource = this.appointmentsBindingSource;
+            this.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay";
+            this.schedulerStorage1.Appointments.Mappings.Description = "Description";
+            this.schedulerStorage1.Appointments.Mappings.End = "EndDate";
+            this.schedulerStorage1.Appointments.Mappings.Label = "Label";
+            this.schedulerStorage1.Appointments.Mappings.Location = "Location";
+            this.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo";
+            this.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo";
+            this.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceID";
+            this.schedulerStorage1.Appointments.Mappings.Start = "StartDate";
+            this.schedulerStorage1.Appointments.Mappings.Status = "Status";
+            this.schedulerStorage1.Appointments.Mappings.Subject = "Subject";
+            this.schedulerStorage1.Appointments.Mappings.Type = "Type";
+            this.schedulerStorage1.Resources.DataSource = this.resourcesBindingSource;
+            this.schedulerStorage1.Resources.Mappings.Caption = "ResourceName";
+            this.schedulerStorage1.Resources.Mappings.Color = "Color";
+            this.schedulerStorage1.Resources.Mappings.Id = "ResourceID";
+            this.schedulerStorage1.AppointmentsChanged += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(this.OnApptChangedInsertedDeleted);
+            this.schedulerStorage1.AppointmentsInserted += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(this.OnApptChangedInsertedDeleted);
+            this.schedulerStorage1.AppointmentsDeleted += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(this.OnApptChangedInsertedDeleted);
+            // 
+            // appointmentsBindingSource
+            // 
+            this.appointmentsBindingSource.DataMember = "Appointments";
+            this.appointmentsBindingSource.DataSource = this.xtraSchedulingDataSet;
+            // 
+            // xtraSchedulingDataSet
+            // 
+            this.xtraSchedulingDataSet.DataSetName = "XtraSchedulingDataSet";
+            this.xtraSchedulingDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // resourcesBindingSource
+            // 
+            this.resourcesBindingSource.DataMember = "Resources";
+            this.resourcesBindingSource.DataSource = this.xtraSchedulingDataSet1;
+            // 
+            // xtraSchedulingDataSet1
+            // 
+            this.xtraSchedulingDataSet1.DataSetName = "XtraSchedulingDataSet1";
+            this.xtraSchedulingDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // appointmentsTableAdapter
+            // 
+            this.appointmentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // resourcesTableAdapter
+            // 
+            this.resourcesTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Find Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateNavigator1
+            // 
+            this.dateNavigator1.Location = new System.Drawing.Point(625, 158);
+            this.dateNavigator1.Name = "dateNavigator1";
+            this.dateNavigator1.SchedulerControl = this.schedulerControl1;
+            this.dateNavigator1.Size = new System.Drawing.Size(179, 316);
+            this.dateNavigator1.TabIndex = 3;
+            this.dateNavigator1.View = DevExpress.XtraEditors.Controls.DateEditCalendarViewType.MonthInfo;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.slotDuration);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(625, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Free Time Slots in WorkTime";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Duration (Hours:Minutes):";
+            // 
+            // slotDuration
+            // 
+            this.slotDuration.EditValue = new System.DateTime(2008, 9, 26, 1, 30, 0, 0);
+            this.slotDuration.Location = new System.Drawing.Point(10, 38);
+            this.slotDuration.Name = "slotDuration";
+            this.slotDuration.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.slotDuration.Properties.EditValueChangedFiringMode = DevExpress.XtraEditors.Controls.EditValueChangedFiringMode.Default;
+            this.slotDuration.Properties.Mask.EditMask = "HH:mm";
+            this.slotDuration.Size = new System.Drawing.Size(107, 20);
+            this.slotDuration.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 486);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateNavigator1);
+            this.Controls.Add(this.schedulerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraSchedulingDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraSchedulingDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.slotDuration.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private XtraSchedulingDataSet xtraSchedulingDataSet;
+        private System.Windows.Forms.BindingSource appointmentsBindingSource;
+        private FreeTimeIntervals.XtraSchedulingDataSetTableAdapters.AppointmentsTableAdapter appointmentsTableAdapter;
+        private XtraSchedulingDataSet1 xtraSchedulingDataSet1;
+        private System.Windows.Forms.BindingSource resourcesBindingSource;
+        private FreeTimeIntervals.XtraSchedulingDataSet1TableAdapters.ResourcesTableAdapter resourcesTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private DevExpress.XtraScheduler.DateNavigator dateNavigator1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.TimeEdit slotDuration;
+    }
+}
+
---
+++ VB/FreeTimeIntervals/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler
+Imports DevExpress.XtraScheduler.Tools
+
+Namespace FreeTimeIntervals
+	Partial Public Class Form1
+		Inherits Form
+		Private timeZoneHelper_Renamed As TimeZoneHelper
+		' Specify non-working time interval.
+		Private nonWorkingTime_Renamed As New TimeOfDayInterval(TimeSpan.FromHours(18), TimeSpan.FromDays(1) + TimeSpan.FromHours(9))
+
+		Friend ReadOnly Property TimeZoneHelper() As TimeZoneHelper
+			Get
+				Return timeZoneHelper_Renamed
+			End Get
+		End Property
+		Friend Property NonWorkingTime() As TimeOfDayInterval
+			Get
+				Return nonWorkingTime_Renamed
+			End Get
+			Set(ByVal value As TimeOfDayInterval)
+				nonWorkingTime_Renamed = value
+			End Set
+		End Property
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' TODO: This line of code loads data into the 'xtraSchedulingDataSet1.Resources' table. You can move, or remove it, as needed.
+			Me.resourcesTableAdapter.Fill(Me.xtraSchedulingDataSet1.Resources)
+			' TODO: This line of code loads data into the 'xtraSchedulingDataSet.Appointments' table. You can move, or remove it, as needed.
+			Me.appointmentsTableAdapter.Fill(Me.xtraSchedulingDataSet.Appointments)
+
+			timeZoneHelper_Renamed = New TimeZoneHelper(Me.schedulerControl1.OptionsBehavior.ClientTimeZoneId)
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim duration As TimeSpan = (CDate(slotDuration.EditValue)).TimeOfDay
+			Dim start As DateTime = schedulerControl1.ActiveView.SelectedInterval.End
+			Dim startOfWeek As DateTime = DevExpress.XtraScheduler.Native.DateTimeHelper.GetStartOfWeek(start)
+			Dim interval As New TimeInterval(start, startOfWeek.AddDays(7))
+			Dim freeTime As TimeInterval = FindInterval(interval, duration)
+			Dim text As String
+			If TimeInterval.Equals(freeTime, TimeInterval.Empty) Then
+				text = "Not found"
+			Else
+				Dim clientFreeInterval As TimeInterval = TimeZoneHelper.ToClientTime(freeTime)
+				text = "Free time interval equal to " & clientFreeInterval.Duration.ToString() & " is found! " & Constants.vbLf + Constants.vbCr & " It starts on " & clientFreeInterval.Start.Date.ToShortDateString() & " at " & clientFreeInterval.Start.TimeOfDay.ToString() & "."
+				schedulerControl1.ActiveView.SetSelection(clientFreeInterval, Resource.Empty)
+			End If
+			MessageBox.Show(text, "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Information)
+		End Sub
+		Private Function FindInterval(ByVal clientInterval As TimeInterval, ByVal duration As TimeSpan) As TimeInterval
+			Dim calculator As New FreeTimeCalculator(schedulerControl1.Storage)
+			' Set a handler for the IntervalFound event.
+			AddHandler calculator.IntervalFound, AddressOf OnIntervalFound
+			Dim interval As TimeInterval = TimeZoneHelper.FromClientTime(clientInterval)
+			' Call the method which raises the event.
+			Return calculator.FindFreeTimeInterval(interval, duration, True)
+		End Function
+		Private Sub OnIntervalFound(ByVal sender As Object, ByVal args As IntervalFoundEventArgs)
+			Dim freeIntervals As TimeIntervalCollectionEx = args.FreeIntervals
+			Dim start As DateTime = freeIntervals.Start.Date.AddDays(-1)
+			Dim [end] As DateTime = freeIntervals.End
+			Do While start < [end]
+				RemoveNonWorkingTime(freeIntervals, start)
+				RemoveNonWorkingDay(freeIntervals, start)
+				start = start + TimeSpan.FromDays(1)
+			Loop
+		End Sub
+		Private Sub RemoveNonWorkingTime(ByVal freeIntervals As TimeIntervalCollectionEx, ByVal [date] As DateTime)
+			Dim clientDate As DateTime = TimeZoneHelper.ToClientTime([date]).Date
+
+			Dim clientNonWorkingTime As New TimeInterval(clientDate + NonWorkingTime.Start, clientDate + NonWorkingTime.End)
+			freeIntervals.Remove(TimeZoneHelper.FromClientTime(clientNonWorkingTime))
+		End Sub
+		Private Sub RemoveNonWorkingDay(ByVal freeIntervals As TimeIntervalCollectionEx, ByVal [date] As DateTime)
+			Dim clientDate As DateTime = TimeZoneHelper.ToClientTime([date]).Date
+			Dim isWorkDay As Boolean = schedulerControl1.WorkDays.IsWorkDay(clientDate)
+			If (Not isWorkDay) Then
+				Dim clientInterval As New TimeInterval(clientDate, TimeSpan.FromDays(1))
+				freeIntervals.Remove(TimeZoneHelper.FromClientTime(clientInterval))
+			End If
+		End Sub
+		Private Sub OnApptChangedInsertedDeleted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsChanged, schedulerStorage1.AppointmentsInserted, schedulerStorage1.AppointmentsDeleted
+			appointmentsTableAdapter.Update(xtraSchedulingDataSet)
+			xtraSchedulingDataSet.AcceptChanges()
+		End Sub
+	End Class
+End Namespace
